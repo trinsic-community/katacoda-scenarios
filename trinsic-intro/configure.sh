f9330f3674d14808cd61751205cac5fa75d5d34a
Tomislav Markovski <tmarkovski@gmail.com>
2022-04-27 16:32:50 -0700

    Update index.json

--- trinsic-intro/configure.sh
+++ trinsic-intro/configure.sh
@@ -2,10 +2,10 @@
 
 apt update && apt install -y wget
 
-wget https://github.com/trinsic-id/sdk/releases/download/v1.4.0/trinsic_1.4.0_amd64.deb
+wget https://github.com/trinsic-id/sdk/releases/download/v1.4.0/trinsic_1.4.2_amd64.deb
 
-dpkg -i trinsic_1.4.0_amd64.deb
+dpkg -i trinsic_1.4.2_amd64.deb
 
-rm trinsic_1.4.0_amd64.deb
+rm trinsic_1.4.2_amd64.deb
 
 echo "done" >> /root/katacoda-background-finished
--- trinsic-intro/ready.sh
+++ trinsic-intro/ready.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 install.sh
+cd example
+clear
